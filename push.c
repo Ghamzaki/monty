28d9dac65015d4aab2126e671aca15d5fb07e954
Ghamzaki <uthmanghamzaki@gmail.com>
2023-12-14 19:32:31 +0000

    sack and queues

---
+++ push.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * push - pushes an element to the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number in the file
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node = malloc(sizeof(stack_t));
+	char *arg;
+	int value;
+
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	arg = strtok(NULL, "\t\n");
+	if (!arg || !isdigit(*arg) && *arg != '-')
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		free(new_node);
+		exit(EXIT_FAILURE);
+	}
+
+	value = atoi(arg);
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new_node;
+	*stack = new_node;
+}
